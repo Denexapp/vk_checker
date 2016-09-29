f9fe67b33358a19dfdf5be32ac68ce1567d67714
Denis Mukhametov <denis.mukhametov@hmail.com>
2016-09-30 02:41:22 +0500

    wrap api calls in handle_captcha

--- app.py
+++ app.py
@@ -1,19 +1,31 @@
 # coding=utf-8
 import vk
 import time
-import random
 import os
 import requests
 
 
+def handle_captcha(function, *args, **kwargs):
+    try:
+        return function(*args, **kwargs)
+    except vk.exceptions.VkAPIError as e:
+        sid = e.captcha_sid
+        img = e.captcha_img
+        print("Captcha img available at {}".format(img))
+        time.sleep(60)
+        key = requests.get(os.environ["captcha_solution_url"]).content
+        print("Captcha key is {}".format(key))
+        return function(*args, **kwargs, captcha_key=key, captcha_sid=sid)
+
+
 def get_status(target):
-    response = api.users.get(user_ids=target, fields="status")[0]
+    response = handle_captcha(api.users.get, user_ids=target, fields="status")[0]
     return response["status"]
 
 
 def send_message(message, target):
-    api.messages.send(peer_id=target, random_id=random.randint(0, 1024),
-                      message=message, v="5.53")
+    handle_captcha(api.messages.send, user_id=target,
+                      message=message, v="4.104")
 
 # you should define vars in your environment
 # or simply replace os.environ() calls
@@ -28,18 +40,7 @@ target = "id" + str(target)
 access_token = os.environ["access_token"]
 session = vk.Session(access_token=access_token)
 api = vk.API(session)
-target_info = None
-try:
-    target_info = api.users.get(user_ids=target, fields="sex")[0]
-except vk.exceptions.VkAPIError as e:
-    time.sleep(1)
-    sid = e.captcha_sid
-    img = e.captcha_img
-    print("Captcha img available at {}".format(img))
-    time.sleep(60)
-    key = requests.get(os.environ["captcha_solution_url"]).content
-    print("Captcha key is {}".format(key))
-    target_info = api.users.get(user_ids=target, fields="sex", captcha_key=key, captcha_sid=sid)[0]
+target_info = handle_captcha(api.users.get, user_ids=target, fields="sex")[0]
 target_name = target_info["first_name"] + " " + target_info["last_name"]
 target_gender = target_info["sex"] == 1
 print(target_info)
