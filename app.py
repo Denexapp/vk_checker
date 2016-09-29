58b7baba58c73880992f975fef78878cc3d88694
Denis Mukhametov <denis.mukhametov@hmail.com>
2016-09-29 22:29:46 +0500

    initial commit

---
+++ app.py
@@ -0,0 +1,43 @@
+# coding=utf-8
+import vk
+import time
+import random
+import os
+
+
+def get_status(target):
+    response = api.users.get(user_ids=target, fields="status")[0]
+    return response["status"]
+
+
+def send_message(message, target):
+    api.messages.send(peer_id=target, random_id=random.randint(0, 1024),
+                      message=message, v="5.53")
+
+# you should define vars in your environment
+# or simply replace os.environ() calls
+# by actual values
+
+# there should be id of people, example:
+# target = 12345
+# listener = 54321
+target = os.environ['target']
+listener = os.environ['listener']
+target = "id" + str(target)
+# your login and password, also requires app_id
+session = vk.AuthSession(app_id=os.environ['app_id'],
+                         user_login=os.environ['login'],
+                         user_password=os.environ['password'],
+                         scope=4096)
+api = vk.API(session)
+target_info = api.users.get(user_ids=target, fields="sex")[0]
+target_name = target_info["first_name"] + " "+ target_info["last_name"]
+target_gender = target_info["sex"] == 1
+print(target_info)
+old_status = None
+while True:
+    status = get_status(target)
+    if status != old_status:
+        old_status = status
+        send_message("{} изменил{} свой статус на \"{}\"".format(target_name, "а" if target_gender else "", status), listener)
+    time.sleep(10)
